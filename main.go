fab4533486379a1c5bccaae8d81057a5a9ac61f5
zinirun <heojj97@naver.com>
2021-06-15 23:19:49 +0900

    modify: socket sources

--- main.go
+++ main.go
@@ -2,31 +2,38 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/zinirun/go-react-chat/pkg/websocket"
 )
 
-func serveWs(w http.ResponseWriter, r *http.Request) {
-	ws, err := websocket.Upgrade(w, r)
-
+func serveWs(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
+	fmt.Println("WebSocket Endpoint Hit")
+	conn, err := websocket.Upgrade(w, r)
 	if err != nil {
-		log.Println(err)
+		fmt.Fprintf(w, "%+v\n", err)
 	}
-	go websocket.Writer(ws)
-	websocket.Reader(ws)
+
+	client := &websocket.Client{
+		Conn: conn,
+		Pool: pool,
+	}
+
+	pool.Register <- client
+	client.Read()
 }
 
 func setupRoutes() {
-	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	// 	fmt.Fprintf(w, "Simple Server")
-	// })
-	http.HandleFunc("/ws", serveWs)
+	pool := websocket.NewPool()
+	go pool.Start()
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(pool, w, r)
+	})
 }
 
 func main() {
+	fmt.Println("Go Chat v0.1")
 	setupRoutes()
 	http.ListenAndServe(":8080", nil)
-	fmt.Println("Server listening on port 8080")
 }
--- pkg/websocket/websocket.go
+++ pkg/websocket/websocket.go
@@ -1,8 +1,6 @@
 package websocket
 
 import (
-	"fmt"
-	"io"
 	"log"
 	"net/http"
 
@@ -26,43 +24,43 @@ func Upgrade(w http.ResponseWriter, r *http.Request) (*websocket.Conn, error) {
 	return ws, nil
 }
 
-func Reader(conn *websocket.Conn) {
-	for {
-		messageType, p, err := conn.ReadMessage()
-		if err != nil {
-			log.Println(err)
-			return
-		}
+// func Reader(conn *websocket.Conn) {
+// 	for {
+// 		messageType, p, err := conn.ReadMessage()
+// 		if err != nil {
+// 			log.Println(err)
+// 			return
+// 		}
 
-		fmt.Println(string(p))
+// 		fmt.Println(string(p))
 
-		if err := conn.WriteMessage(messageType, p); err != nil {
-			log.Println(err)
-			return
-		}
-	}
-}
+// 		if err := conn.WriteMessage(messageType, p); err != nil {
+// 			log.Println(err)
+// 			return
+// 		}
+// 	}
+// }
 
-func Writer(conn *websocket.Conn) {
-	for {
-		fmt.Println("Sending")
-		messageType, r, err := conn.NextReader()
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		w, err := conn.NextWriter(messageType)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		if _, err := io.Copy(w, r); err != nil {
-			fmt.Println(err)
-			return
-		}
-		if err := w.Close(); err != nil {
-			fmt.Println(err)
-			return
-		}
-	}
-}
+// func Writer(conn *websocket.Conn) {
+// 	for {
+// 		fmt.Println("Sending")
+// 		messageType, r, err := conn.NextReader()
+// 		if err != nil {
+// 			fmt.Println(err)
+// 			return
+// 		}
+// 		w, err := conn.NextWriter(messageType)
+// 		if err != nil {
+// 			fmt.Println(err)
+// 			return
+// 		}
+// 		if _, err := io.Copy(w, r); err != nil {
+// 			fmt.Println(err)
+// 			return
+// 		}
+// 		if err := w.Close(); err != nil {
+// 			fmt.Println(err)
+// 			return
+// 		}
+// 	}
+// }
